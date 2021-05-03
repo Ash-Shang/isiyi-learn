206d94f99f02874387477e96195451406ffd0e00
siyi <ash159@qq.com>
2021-05-03 20:10:29 +0800

    FileChannel的测试用例

---
+++ isiyi-netty/src/main/java/com/isiyi/nio/buffer/BufferTest.java
@@ -0,0 +1,20 @@
+package com.isiyi.nio.buffer;
+
+import java.nio.Buffer;
+
+/**
+ * 类描述
+ * <p></p>
+ *
+ * @version 1.0.0
+ * @description: BufferTest
+ * @author: 向鹏飞
+ * @since: 2021/5/1
+ */
+public class BufferTest {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ isiyi-netty/src/main/java/com/isiyi/nio/channel/FileChannelTest.java
@@ -0,0 +1,124 @@
+package com.isiyi.nio.channel;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * 本地文件读写
+ * <p></p>
+ *
+ * @version 1.0.0
+ * @description: FileChannelTest01
+ * @author: 向鹏飞
+ * @since: 2021/5/3
+ */
+public class FileChannelTest {
+
+
+    @Test
+    public void testWrite() throws Exception {
+        String str = "hello,FileChannel";
+        //创建一个输出流 -> channel
+        FileOutputStream fileOutputStream = new FileOutputStream("d:\\file01.txt");
+
+        //通过 fileOutputStream 获取对应的 FileChannel
+        //这个 fileChannel 真实类型是 FileChannelImpl
+        FileChannel fileChannel = fileOutputStream.getChannel();
+
+        //创建一个缓冲区 ByteBuffer
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+
+        //将 str 放入 byteBuffer
+        byteBuffer.put(str.getBytes());
+
+        //对 byteBuffer 进行 flip
+        byteBuffer.flip();
+
+        //将 byteBuffer 数据写入到 fileChannel
+        fileChannel.write(byteBuffer);
+        fileOutputStream.close();
+    }
+
+    @Test
+    public void testRead() throws Exception{
+        //创建文件的输入流
+        File file = new File("d:\\file01.txt");
+        FileInputStream fileInputStream = new FileInputStream(file);
+
+        //通过 fileInputStream 获取对应的 FileChannel -> 实际类型 FileChannelImpl
+        FileChannel fileChannel = fileInputStream.getChannel();
+
+        //创建缓冲区
+        ByteBuffer byteBuffer = ByteBuffer.allocate((int)file.length());
+
+        //将通道的数据读入到 Buffer
+        fileChannel.read(byteBuffer);
+
+        //将 byteBuffer 的字节数据转成 String
+        System.out.println(new String(byteBuffer.array()));
+        fileInputStream.close();
+    }
+
+    @Test
+    public void testOneBufferToWriteAndRead() throws Exception{
+        FileInputStream fileInputStream = new FileInputStream("src/main/1.txt");
+        FileChannel fileChannel01 = fileInputStream.getChannel();
+        FileOutputStream fileOutputStream = new FileOutputStream("2.txt");
+        FileChannel fileChannel02 = fileOutputStream.getChannel();
+
+        ByteBuffer byteBuffer = ByteBuffer.allocate(512);
+
+        while (true) { //循环读取
+
+            //这里有一个重要的操作，一定不要忘了
+            /*
+            public final Buffer clear() {
+                position = 0;
+                limit = capacity;
+                mark = -1;
+                return this;
+            }
+            */
+            byteBuffer.clear(); //清空 buffer
+            int read = fileChannel01.read(byteBuffer);
+            System.out.println("read = " + read);
+            if (read == -1) { //表示读完
+                break;
+            }
+
+            //将 buffer 中的数据写入到 fileChannel02--2.txt
+            byteBuffer.flip();
+            fileChannel02.write(byteBuffer);
+        }
+
+        //关闭相关的流
+        fileInputStream.close();
+        fileOutputStream.close();
+    }
+
+    @Test
+    public void testTransferFrom() throws Exception {
+        //创建相关流
+        FileInputStream fileInputStream = new FileInputStream("d:\\8cb9450aaa.jpg");
+        FileOutputStream fileOutputStream = new FileOutputStream("d:\\a2.jpg");
+
+        //获取各个流对应的 FileChannel
+        FileChannel sourceCh = fileInputStream.getChannel();
+        FileChannel destCh = fileOutputStream.getChannel();
+
+        //使用 transferForm 完成拷贝
+        destCh.transferFrom(sourceCh, 0, sourceCh.size());
+
+        //关闭相关通道和流
+        sourceCh.close();
+        destCh.close();
+        fileInputStream.close();
+        fileOutputStream.close();
+    }
+
+}
