dded4437af81ee546bcb9910a4ce648e9a8381bf
siyi <ash159@qq.com>
2021-07-05 11:58:47 +0800

    Canal

---
+++ isiyi-canal/src/main/java/com/isiyi/canal/CanalApplication.java
@@ -0,0 +1,20 @@
+package com.isiyi.canal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 类描述
+ * <p></p>
+ *
+ * @version 1.0.0
+ * @description: CanalApplication
+ * @author: 向鹏飞
+ * @since: 2021/7/5
+ */
+@SpringBootApplication
+public class CanalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CanalApplication.class, args);
+    }
+}
---
+++ isiyi-canal/src/main/java/com/isiyi/canal/SimpleCanalClientExample.java
@@ -0,0 +1,113 @@
+package com.isiyi.canal;
+
+import com.alibaba.otter.canal.client.CanalConnector;
+import com.alibaba.otter.canal.client.CanalConnectors;
+import com.alibaba.otter.canal.common.utils.AddressUtils;
+import com.alibaba.otter.canal.protocol.CanalEntry.*;
+import com.alibaba.otter.canal.protocol.Message;
+import org.junit.Test;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+import java.net.InetSocketAddress;
+import java.util.List;
+
+/**
+ * 类描述
+ * <p></p>
+ *
+ * @version 1.0.0
+ * @description: SimpleCanalClientExample
+ * @author: 向鹏飞
+ * @since: 2021/7/5
+ */
+@Component
+public class SimpleCanalClientExample implements InitializingBean {
+
+    public void main() {
+        // 创建链接
+        CanalConnector connector = CanalConnectors.newSingleConnector(new InetSocketAddress("127.0.0.1",
+                11111), "example", "", "");
+        int batchSize = 1000;
+        try {
+            //打开连接
+            connector.connect();
+            //订阅数据库表,全部表
+            connector.subscribe(".*\\..*");
+            //回滚到未进行ack的地方，下次fetch的时候，可以从最后一个没有ack的地方开始拿
+            connector.rollback();
+            while (true) {
+                // 获取指定数量的数据
+                Message message = connector.getWithoutAck(batchSize); // 获取指定数量的数据
+                //获取批量ID
+                long batchId = message.getId();
+                //获取批量的数量
+                int size = message.getEntries().size();
+                //没有数据
+                if (batchId == -1 || size == 0) {
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                    }
+                } else {
+                    //如果有数据,处理数据
+                    printEntry(message.getEntries());
+                }
+                //进行 batch id 的确认。确认之后，小于等于此 batchId 的 Message 都会被确认。
+                connector.ack(batchId);
+                // connector.rollback(batchId); // 处理失败, 回滚数据
+            }
+        } finally {
+            connector.disconnect();
+        }
+    }
+
+    private static void printEntry(List<Entry> entrys) {
+        for (Entry entry : entrys) {
+            if (entry.getEntryType() == EntryType.TRANSACTIONBEGIN || entry.getEntryType() == EntryType.TRANSACTIONEND) {
+                //开启/关闭事务的实体类型，跳过
+                continue;
+            }
+            //RowChange对象，包含了一行数据变化的所有特征
+            //比如isDdl 是否是ddl变更操作 sql 具体的ddl sql beforeColumns afterColumns 变更前后的数据字段等等
+            RowChange rowChage = null;
+            try {
+                rowChage = RowChange.parseFrom(entry.getStoreValue());
+            } catch (Exception e) {
+                throw new RuntimeException("ERROR ## parser of eromanga-event has an error , data:" + entry.toString(),
+                        e);
+            }
+            //获取操作类型：insert/update/delete类型
+            EventType eventType = rowChage.getEventType();
+            System.out.println(String.format("================&gt; binlog[%s:%s] , name[%s,%s] , eventType : %s",
+                    entry.getHeader().getLogfileName(), entry.getHeader().getLogfileOffset(),
+                    entry.getHeader().getSchemaName(), entry.getHeader().getTableName(),
+                    eventType));
+
+            for (RowData rowData : rowChage.getRowDatasList()) {
+                if (eventType == EventType.DELETE) {
+                    printColumn(rowData.getBeforeColumnsList());
+                } else if (eventType == EventType.INSERT) {
+                    printColumn(rowData.getAfterColumnsList());
+                } else {
+                    System.out.println("-------&gt; before");
+                    printColumn(rowData.getBeforeColumnsList());
+                    System.out.println("-------&gt; after");
+                    printColumn(rowData.getAfterColumnsList());
+                }
+            }
+        }
+    }
+
+    private static void printColumn(List<Column> columns) {
+        for (Column column : columns) {
+            System.out.println(column.getName() + " : " + column.getValue() + "    update=" + column.getUpdated());
+        }
+    }
+
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        main();
+    }
+}
